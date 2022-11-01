81db60593e76905ac3e00b603e272cfd578eb9c0
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-01 12:48:55 +0530

    Added main file

--- Line_Comparision_Problem/Program.cs
+++ Line_Comparision_Problem/Program.cs
@@ -19,20 +19,7 @@
 
             double LineLength1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine($"Length of Line 1 is : {LineLength1}");
-            //int l1, l2, m1, m2;
-
-            //Console.WriteLine("Please Enter l1:");
-            //l1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Please Enter l2:");
-            //l2 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Please Enter m1:");
-            //m1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Please Enter m2:");
-            //m2 = Convert.ToInt32(Console.ReadLine());
-
-            //double LineLength2 = Math.Sqrt(Math.Pow(l2 - l1, 2) + Math.Pow(m2 - m1, 2));
-            //Console.WriteLine($"Length of Line 2 is : {LineLength2}");
-
+            
         }
     }
 }
